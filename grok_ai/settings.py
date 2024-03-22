082a2b2c6603d33f5a6c2a037a6a5a53a1ab71df
imran116 <ih9114101@gmail.com>
2024-03-22 18:07:47 +0600

    v1.2: add static conf

--- grok_ai/settings.py
+++ grok_ai/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import django_heroku
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- grok_ai/urls.py
+++ grok_ai/urls.py
@@ -1,8 +1,9 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chatApp.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('chatApp.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
